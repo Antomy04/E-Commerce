478cffd3b1302e835bb6d3beda5e58a3c3fcd15d
kk <kksingh2228@gmail.com>
2026-04-17 00:19:59 +0530

    path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 const connectDB = require('./backend/config/db');
 const { notFound, errorHandler } = require('./backend/middleware/errorMiddleware');
 
